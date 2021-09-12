570d24c03c6386f439e2bc5dece9c8b2a21dddb7
Nahuel M <45260978+yayo12@users.noreply.github.com>
2021-09-12 18:16:47 -0300

    Update main.js

--- v0/main.js
+++ v0/main.js
@@ -1,12 +1,19 @@
-const nuevastareas = document.getElementById("nuevastareas");
-const botonag = document.getElementById("botonag");
+const nuevatarea = document.getElementById("nuevastareas");
+const boton = document.getElementById("botonag");
 const lista = document.getElementById("lista");
 
-botonag.addEventListener("click", function(n){
+boton.addEventListener("click", function(n){
 n.preventDefault();
-const textodetarea = nuevastareas.value
-agregartarea(textodetarea);
+var textodetarea = nuevatarea.value;
+var nuevoli = document.createElement("li");
+nuevoli.innerHTML = `
+<input type="checkbox">
+    <p>${textodetarea}</p>
+    <button onclick="eliminarTarea(this)">Eliminar</button>
+`;
+lista.prepend(nuevoli)
 
-
+});
+function eliminarTarea(n) {
+    n.parentElement.remove(); //elimina el elemento padre del elemento que se le pasa como parámetro
 }
-);
